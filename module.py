ba050418da6e1f82797c79128d398ca188d5def8
Andrew Murray <andyranted@gmail.com>
2017-07-12 19:59:09 +0100

    Add initial parsing routines and data classes for ivy format.

---
+++ module.py
@@ -0,0 +1,258 @@
+import xml.etree.ElementTree as ET
+from attrdict import AttrDict
+import logging
+
+
+def else_default(x, y):
+    return y if x is None else x
+
+
+def maybe_map(x, f):
+    return f(x) if x is not None else None
+
+
+"""
+
+http://ant.apache.org/ivy/history/2.0.0/ivyfile/conf.html
+
++--------------+-----------------------------------------------------------------------------------------------+-------------------------+
+|  Attribute   |                                     Description                                               |       Required          |
++--------------+-----------------------------------------------------------------------------------------------+-------------------------+
+| name         | the name of the declared configuration                                                        |  Yes                    |
+| description  | a description for the declared configuration                                                  |  No                     |
+| visibility   | the visibility of the declared configuration. 'public' means that this configuration can      |                         |
+|              | be used by other modules, while 'private' means that this configuration is used only in       |  No, defaults to public |
+|              | the module itself, and is not exposed to other modules.                                       |                         |
+| extends      | a comma separated list of configurations of this module that the current configuration extends|  No, defaults to none   |
+| transitive   | a boolean to indicate if this conf is transitive or not since 1.4                             |  No, defaults to true   |
+| deprecated   | indicates that this conf has been deprecated by giving the date of the deprecation.           |  No, by default the     |
+|              | It should be given in this format: yyyyMMddHHmmss                                             |  conf is not deprecated |
++----------------------------------------------------------------------------------------------------------------------------------------+
+"""
+
+
+class conf(object):
+
+    def __init__(
+        self,
+        name,
+        description=None,
+        visibility="public",
+        extends=None,
+        transitive=True,
+        deprecated=None
+    ):
+        self.name = name
+        self.description = description
+        self.visibility = else_default(visibility, "public")
+        self.extends = extends if extends else []
+        self.transitive = else_default(transitive, True)
+        self.deprecated = deprecated
+
+    @staticmethod
+    def from_node(node):
+        return conf(
+            node.get("name"),
+            node.get("description"),
+            node.get("visibility"),
+            maybe_map(
+                node.get("extends"),
+                lambda k: [x.strip() for x in k.split(",")]
+            ),
+            node.get("transitive"),
+            node.get("deprecated")
+        )
+
+
+"""
+http://ant.apache.org/ivy/history/2.0.0/ivyfile/dependency.html
+
++---------------+---------------------------------------------------------------------------------------------------+-----------------------------------------------------------------+
+|   Attribute   |                                        Description                                                |                       Required                                  |
++---------------+---------------------------------------------------------------------------------------------------+-----------------------------------------------------------------+
+| org           | the name of the organisation of the dependency.                                                   | No, defaults to the master module organisation                  |
+| name          | the module name of the dependency                                                                 | Yes                                                             |
+| branch        | the branch of the dependency. since 1.4                                                           | No, defaults to the default branch setting for the dependency.  |
+| rev           | the revision of the dependency. See above for details.                                            | Yes                                                             |
+| revConstraint | the dynamic revision constraint originally used for this dependency. See above for details.       | No, defaults to the value of rev                                |
+| force         | a boolean to give an indication to conflict manager that this dependency                          |                                                                 |
+|               | should be forced to this revision (see conflicts manager)                                         | No, defaults to false                                           |                                                                                                                            |
+| conf          | an inline mapping configuration spec (see above for details)                                      | No, defaults to defaultconf attribute of dependencies element   |
+|               |                                                                                                   | if neither conf attribute nor conf children element is given    |
+| transitive    | true to resolve this dependency transitively, false otherwise (since 1.2)                         | No, defaults to true                                            |
+| changing      | true if the dependency artifacts may change without revision change, false otherwise (since 1.2). | No, defaults to false                                           |
+|               | See cache and change management for details.                                                      |                                                                 |
++---------------+---------------------------------------------------------------------------------------------------+-----------------------------------------------------------------+
+"""
+
+
+class dependency(object):
+
+    def __init__(
+        self,
+        org,
+        name,
+        branch,
+        rev,
+        revConstraint,
+        conf,
+        force=False,
+        transitive=True,
+        changing=False
+    ):
+        self.org = org
+        self.name = name
+        self.branch = branch
+        self.rev = rev
+        self.revConstraint = revConstraint
+        self.conf = conf
+        self.force = else_default(force, False)
+        self.transitive = else_default(transitive, True)
+        self.changing = else_default(changing, False)
+
+    @staticmethod
+    def from_node(node):
+        # at the moment, we canonicalise into a string representation
+        # a little messy ... they should be canonicalised to
+        # a set of mappings ?
+        # lists of pairs of strings ?
+        conf_nodes = list(node.findall("conf"))
+
+        def parse_sub_conf(cn):
+            src = cn.get("name")
+            sub_mapped_nodes = list(cn.findall("mapped"))
+            mapping_from_nodes = ",".join(
+                mn.get("name") for mn in sub_mapped_nodes
+            )
+            mapping_from_attributes = cn.get("mapped")
+            return src + "->" + mapping_from_nodes + else_default(mapping_from_attributes, "")
+
+        conf_from_nodes = ";".join(parse_sub_conf(cn) for cn in conf_nodes)
+        conf_from_attr = else_default(node.get("conf"), "")
+
+        print "configs"
+        print conf_from_nodes
+        print conf_from_attr
+
+        if conf_from_attr and conf_from_nodes:
+            conf = conf_from_attr + ";" + conf_from_nodes
+        elif not conf_from_nodes and not conf_from_attr:
+            conf = None
+        else:
+            # one of them is valid
+            # concat without seperator ...
+            # at least one of them will be an empty string
+            conf = conf_from_attr + conf_from_nodes
+
+        # we ignore some features
+        # notably artifact, include, exclude
+        # these three features are designed for "specifying an ivy structure
+        # for a dependency that doesn't have an ivy file" ... this is obviosly
+        # convenient, but I currently don't use this, and mimicking this
+        # feature faithfully would rather complicate matters
+
+        artifacts = list(node.findall("artifact"))
+        includes = list(node.findall("include"))
+        excludes = list(node.findall("exclude"))
+        if artifacts or includes or excludes:
+            logging.warning(
+                "ira does not support artifact declarations "
+                "inside dependencies. <artifact>, <include> and <exclude> "
+                "will be ignored in this context."
+            )
+
+        return dependency(
+            node.get("org"),
+            node.get("name"),
+            node.get("branch"),
+            node.get("rev"),
+            node.get("revConstraint"),
+            conf,
+            node.get("force"),
+            node.get("transitive"),
+            node.get("changing")
+        )
+
+
+"""
+http://ant.apache.org/ivy/history/2.0.0/ivyfile/artifact.html
+
++-----------+---------------------------------------------------------------------------------------+-----------------------------------------------------------------------------+
+| Attribute |                                      Description                                      |                                  Required                                   |
++-----------+---------------------------------------------------------------------------------------+-----------------------------------------------------------------------------+
+| name      | the name of the published artifact. This name must not include revision.              | No, defaults to the name of the module                                      |
+| type      | the type of the published artifact. It's usually its extension, but not necessarily.  | No, defaults to jar                                                         |
+|           | For instance, ivy files are of type 'ivy' but have 'xml' extension                    |                                                                             |
+| ext       | the extension of the published artifact                                               | No, defaults to type                                                        |
+| conf      | comma separated list of public configurations in which this artifact is published.    |                                                                             |
+|           | '*' wildcard can be used to designate all public configurations of this module        | No, defaults to defaultconf attribute value on parent publications element. |
+| url       | a url at which this artifact can be found if it isn't located at the standard         | No, defaults to no url                                                      |
+|           | location in the repository since 1.4                                                  |                                                                             |
++-----------+---------------------------------------------------------------------------------------+-----------------------------------------------------------------------------+
+"""
+
+
+class artifact(object):
+    def __init__(
+        self,
+        name,
+        type_,
+        ext,
+        conf,
+        url
+    ):
+        self.name = name
+        self.type = type_
+        self.ext = ext
+        self.conf = conf
+        self.url = url
+
+    @staticmethod
+    def from_node(node):
+        # publication supports child conf elements, to indicate how it has
+        # been published, or as an attribute on the tag
+        # this parse, should support both simultaneously, though this would
+        # be a little silly
+        child_configurations = list(
+            subnode.get("name") for subnode in node.findall("conf")
+        )
+        attr_configurations = maybe_map(
+            node.get("conf"),
+            lambda k: [x.strip() for x in k.split(",")]
+        )
+        configurations = (
+            child_configurations + else_default(attr_configurations, [])
+        )
+        return artifact(
+            node.get("name"),
+            node.get("type"),
+            node.get("ext"),
+            configurations,
+            node.get("url")
+        )
+
+
+class module(object):
+    @staticmethod
+    def from_file(filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        configurations = list(
+            conf.from_node(x)
+            for x in root.find("configurations").findall("conf")
+        )
+        dependencies = [
+            dependency.from_node(x)
+            for x in root.find("dependencies").findall("dependency")
+        ]
+        publications = [
+            artifact.from_node(x)
+            for x in root.find("publications").findall("artifact")
+        ]
+
+        return AttrDict({
+            "info": {},
+            "configurations": configurations,
+            "dependencies": dependencies,
+            "publications": publications
+        })
---
+++ tests/test_parse_module.py
@@ -0,0 +1,249 @@
+from ..module import module
+import unittest
+import copy
+
+TEST_FILE = "tests/data/official_sample.xml"
+
+
+class parse_module_test(unittest.TestCase):
+
+    def setUp(self):
+        self.parsed_module = module.from_file(TEST_FILE)
+
+    def test_configurations(self):
+        assert len(self.parsed_module.configurations) == 5
+        """
+    		<conf name="myconf1" description="desc 1"/>
+    		<conf name="myconf2" description="desc 2" visibility="public"/>
+    		<conf name="myconf3" description="desc 3" visibility="private"/>
+    		<conf name="myconf4" description="desc 4" extends="myconf1, myconf2"/>
+    		<conf name="myoldconf" description="my old desc" deprecated="20050115"/>
+        """
+        correct_configurations = [
+            {
+                'name': 'myconf1',
+                'deprecated': None,
+                'extends': [],
+                'transitive': True,
+                'visibility': "public",
+                'description': 'desc 1'
+            },
+            {
+                'name': 'myconf2',
+                'deprecated': None,
+                'extends': [],
+                'transitive': True,
+                'visibility': 'public',
+                'description': 'desc 2'
+            },
+            {
+                'name': 'myconf3',
+                'deprecated': None,
+                'extends': [],
+                'transitive': True,
+                'visibility': 'private',
+                'description': 'desc 3'
+            },
+            {
+                'name': 'myconf4',
+                'deprecated': None,
+                'extends': ["myconf1", "myconf2"],
+                'transitive': True,
+                'visibility': "public",
+                'description': 'desc 4'
+            },
+            {
+                'name': 'myoldconf',
+                'deprecated': '20050115',
+                'extends': [],
+                'transitive': True,
+                'visibility': "public",
+                'description': 'my old desc'
+            }
+        ]
+
+        conf_dicts = [vars(x) for x in self.parsed_module.configurations]
+        for correct, parsed in zip( correct_configurations, conf_dicts):
+            assert correct == parsed
+
+    def test_publications(self):
+        assert len(self.parsed_module.publications) == 4
+        """
+        	<artifact name="myartifact1" type="jar"/>
+        	<artifact name="myartifact2" type="jar" conf="myconf1"/>
+        	<artifact name="myartifact3" type="jar" conf="myconf1, myconf2, myconf3"/>
+        	<artifact name="myartifact4" type="jar">
+        		<conf name="myconf1"/>
+        		<conf name="myconf3"/>
+        	</artifact>
+        """
+        correct_publications = [
+            {
+                'url': None,
+                'ext': None,
+                'type': 'jar',
+                'name': 'myartifact1',
+                'conf': []
+            },
+            {
+                'url': None,
+                'ext': None,
+                'type': 'jar',
+                'name': 'myartifact2',
+                'conf': ['myconf1']
+            },
+            {
+                'url': None,
+                'ext': None,
+                'type': 'jar',
+                'name': 'myartifact3',
+                'conf': ['myconf1', 'myconf2', 'myconf3']
+            },
+            {
+                'url': None,
+                'ext': None,
+                'type': 'jar',
+                'name': 'myartifact4',
+                'conf': ['myconf1', 'myconf3']
+            }
+        ]
+
+        zipped = zip(
+            correct_publications,
+            self.parsed_module.publications
+        )
+        for correct, parsed in zipped:
+            assert correct == vars(parsed)
+
+    def test_dependencies(self):
+        assert len(self.parsed_module.dependencies) == 11
+        """
+    		<dependency name="mymodule2" rev="2.0"/>
+    		<dependency org="yourorg" name="yourmodule1" rev="1.1" conf="myconf1"/>
+    		<dependency org="yourorg" name="yourmodule2" rev="2+" conf="myconf1->yourconf1"/>
+    		<dependency org="yourorg" name="yourmodule3" rev="3.1" conf="myconf1->yourconf1, yourconf2"/>
+    		<dependency org="yourorg" name="yourmodule4" rev="4.1" conf="myconf1, myconf2->yourconf1, yourconf2"/>
+    		<dependency org="yourorg" name="yourmodule5" rev="5.1" conf="myconf1->yourconf1;myconf2->yourconf1, yourconf2"/>
+
+    		<dependency org="yourorg" name="yourmodule6" rev="latest.integration">
+    			<conf name="myconf1" mapped="yourconf1"/>
+    			<conf name="myconf2" mapped="yourconf1, yourconf2"/>
+    		</dependency>
+
+    		<dependency org="yourorg" name="yourmodule7" rev="7.1">
+    			<conf name="myconf1">
+    				<mapped name="yourconf1"/>
+    			</conf>
+    			<conf name="myconf2">
+    				<mapped name="yourconf1"/>
+    				<mapped name="yourconf2"/>
+    			</conf>
+    		</dependency>
+
+    		<dependency org="yourorg" name="yourmodule8" rev="8.1">
+    			<artifact name="yourartifact8-1" type="jar"/>
+    			<artifact name="yourartifact8-2" type="jar"/>
+    		</dependency>
+
+    		<dependency org="yourorg" name="yourmodule9" rev="9.1" conf="myconf1,myconf2,myconf3->default">
+    			<artifact name="yourartifact9-1" type="jar" conf="myconf1,myconf2"/>
+    			<artifact name="yourartifact9-2" type="jar">
+    				<conf name="myconf2"/>
+    				<conf name="myconf3"/>
+    			</artifact>
+    		</dependency>
+
+    		<dependency org="yourorg" name="yourmodule10" rev="10.1">
+    			<include name="your.*" type="jar"/>
+    			<include ext="xml"/>
+    			<exclude name="toexclude"/>
+    		</dependency>
+        """
+
+        default_dep = {
+            'force': False,
+            'conf': None,
+            "revConstraint": None,
+            "branch": None,
+            "changing": False,
+            "transitive": True,
+            "org": None
+        }
+
+        def dep(y):
+            to_mutate = copy.deepcopy(default_dep)
+            to_mutate.update(y)
+            return to_mutate
+
+        # FIXME: Fundamentally, we don't support all the things
+        # associated with dependency tags, in particular when you
+        # specify the ivy structure for an external, this will be ignored
+        # this is a known limitation, and the below test regresses this
+        # behaviour
+        correct_dependencies = [
+            dep({
+                'name': 'mymodule2',
+                'rev': '2.0'
+            }),
+            dep({
+                'name': 'yourmodule1',
+                'conf': 'myconf1',
+                'rev': '1.1',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule2',
+                'conf': 'myconf1->yourconf1',
+                'rev': '2+',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule3',
+                'conf': 'myconf1->yourconf1, yourconf2',
+                'rev': '3.1',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule4',
+                'conf': 'myconf1, myconf2->yourconf1, yourconf2',
+                'rev': '4.1',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule5',
+                'conf': 'myconf1->yourconf1;myconf2->yourconf1, yourconf2',
+                'rev': '5.1',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule6',
+                'rev': 'latest.integration',
+                'org': 'yourorg',
+                'conf': "myconf1->yourconf1;myconf2->yourconf1, yourconf2"
+            }),
+            dep({
+                'name': 'yourmodule7',
+                'rev': '7.1',
+                'org': 'yourorg',
+                'conf': "myconf1->yourconf1;myconf2->yourconf1,yourconf2"
+            }),
+            dep({
+                'name': 'yourmodule8',
+                'rev': '8.1',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule9',
+                'conf': 'myconf1,myconf2,myconf3->default',
+                'rev': '9.1',
+                'org': 'yourorg'
+            }),
+            dep({
+                'name': 'yourmodule10',
+                'rev': '10.1',
+                'org': 'yourorg'
+            })
+        ]
+
+        for correct, parsed in zip(correct_dependencies, self.parsed_module.dependencies):
+            assert correct == vars(parsed)
